033bb47aa4767ac82980b5c9d258494fddc0b851
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-07-15 08:41:56 -0500

    feat: implement product card component and update home page layout

---
+++ src/app/products/components/product-card/product-card.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [RouterLink],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard {}
