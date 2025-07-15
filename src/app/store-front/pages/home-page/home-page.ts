b92e7fc3cc7847aeef046eb425001d47a90d476e
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-07-15 15:37:43 -0500

    feat: add user and product interfaces, implement products service, and update app configuration

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { ProductCard } from "../../../products/components/product-card/product-card";
-
+import { Component, inject } from '@angular/core';
+import { ProductCard } from '@products/components/product-card/product-card';
+import { ProductsService } from '@products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-home-page',
   imports: [ProductCard],
   templateUrl: './home-page.html',
-  styleUrl: './home-page.css'
+  styleUrl: './home-page.css',
 })
 export class HomePage {
+  productsService = inject(ProductsService);
 
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ({ params }) => {
+      return this.productsService.getProducts();
+    },
+  });
 }
