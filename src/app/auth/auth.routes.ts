af4ad52d1f9170bb9aee53ab250bbb8b69b8da07
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-07-28 18:00:32 -0500

    feat: add authentication module with login and registration pages, including routing and layout components

---
+++ src/app/auth/auth.routes.ts
@@ -0,0 +1,26 @@
+import { AuthLayout } from './layout/auth-layout/auth-layout';
+import { LoginPage } from './pages/login-page/login-page';
+import { RegisterPage } from './pages/register-page/register-page';
+
+export const authRoutes = [
+  {
+    path: '',
+    component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        component: LoginPage,
+      },
+      {
+        path: 'register',
+        component: RegisterPage,
+      },
+      {
+        path: '**',
+        redirectTo: 'login',
+      },
+    ],
+  },
+];
+
+export default authRoutes;
