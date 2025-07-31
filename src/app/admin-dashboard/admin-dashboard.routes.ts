1355f20fda4ec7107f73a308957fc454584e517c
AngelCortesM <joseangelcortesmontoya@gmail.com>
2025-07-31 16:10:22 -0500

    feat: implement admin dashboard layout with navigation and user info; add IsAdminGuard for route protection

--- src/app/admin-dashboard/admin-dashboard.routes.ts
+++ src/app/admin-dashboard/admin-dashboard.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { AdminDashboardLayout } from './layouts/admin-dashboard-layout/admin-dashboard-layout';
 import { ProductAdminPage } from './pages/product-admin-page/product-admin-page';
 import { ProductsAdminPage } from './pages/products-admin-page/products-admin-page';
+import { IsAdminGuard } from '@auth/guards/is-admin.guard';
 
 export const adminDashboardRoutes: Routes = [
   {
     path: '',
     component: AdminDashboardLayout,
+    canMatch: [IsAdminGuard],
     children: [
       {
         path: 'products',
@@ -18,8 +20,8 @@ export const adminDashboardRoutes: Routes = [
       },
       {
         path: '**',
-        redirectTo: 'products'
-      }
+        redirectTo: 'products',
+      },
     ],
   },
 ];
